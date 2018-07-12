557146873efdd2b400b0678ccdc95f235bda973c
xiaohongma <565672848@qq.com>
2018-07-12 21:28:41 +0800

    first circle segmation

---
+++ build/CMakeFiles/findcircle.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/findcircle.dir/main.cpp.o"
+  "findcircle.pdb"
+  "findcircle"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/findcircle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
